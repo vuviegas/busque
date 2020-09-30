3dcb6ae8a7a2811a6d0356b2dfc2be1dd58baad5
Mayssaa Khodr <mayus.tayus@gmail.com>
2020-09-30 17:52:53 -0300

    company model implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :companies, dependent: :nullify
+  has_many :companies
 
   validates :email, :role, presence: true
   validates :role, inclusion: { in: %w(admin clerk police),
---
+++ db/migrate/20200930200409_create_companies.rb
@@ -0,0 +1,10 @@
+class CreateCompanies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :companies do |t|
+      t.string :company_name
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
