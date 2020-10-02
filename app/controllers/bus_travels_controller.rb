27979c96350d8083d9719a34fe66c15cb2fa5872
Vinicius Viegas <vuviegas85@gmail.com>
2020-10-01 23:52:57 -0300

    authorizate clerk to see today's travels

--- app/controllers/bus_travels_controller.rb
+++ app/controllers/bus_travels_controller.rb
@@ -1,6 +1,12 @@
 class BusTravelsController < ApplicationController
   def index
-    @bus_travels = BusTravel.where(departure_on: Date.today)
+    if current_user.admin? || current_user.police?
+      @bus_travels = BusTravel.where(departure_on: Date.today)
+    else
+      company = Company.where(user_id: current_user.id)
+      travel_lines = TravelLine.where(company_id: company.ids)
+      @bus_travels = BusTravel.where(departure_on: Date.today, travel_line_id: travel_lines.ids)
+    end
   end
 
   def new
