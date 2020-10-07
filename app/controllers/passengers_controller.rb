57e36552f208c243688118eba51aff28e7944c62
Vinicius Viegas <vuviegas85@gmail.com>
2020-10-07 10:15:24 -0300

    Revisited authorizations and new alert button in passenger show implemented

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,5 +1,5 @@
 class PassengersController < ApplicationController
-  before_action :set_passenger, only: [:show, :edit, :update, :destroy]
+  before_action :set_passenger, only: [:show, :update, :destroy]
 
   def index
     if params[:query].present?
@@ -13,46 +13,78 @@ class PassengersController < ApplicationController
     @passenger_trips = PassengerTrip.where(passenger: @passenger)
   end
 
+  def cpf_check
+    if current_user.admin? || current_user.police?
+      @alert = Alert.new
+    else
+      forbidden
+    end
+  end
+
+  def cpf_check_post
+
+  end
+
   def new
     @passenger = Passenger.new
   end
 
   def create
-    @passenger = Passenger.new(passenger_params)
-    @alert = Alert.new(alert_params)
-    @alert.passenger = @passenger
-    @alert.user = current_user
-
-    if @passenger.save
-      @alert.save
-      redirect_to passenger_path(@passenger), notice: "Passageiro e alerta criados com sucesso!"
+    if current_user.admin? || current_user.police?
+      @passenger = Passenger.new(passenger_params)
+      @alert = Alert.new(alert_params)
+      @alert.passenger = @passenger
+      @alert.user = current_user
+      @alert.solved = false
+      if @passenger.save
+        @alert.save
+        redirect_to passenger_path(@passenger), notice: "Passageiro e alerta criados com sucesso!"
+      else
+        render :new
+      end
     else
-      render :new
+      forbidden
     end
   end
 
   def edit
+    if current_user.admin? || current_user.police?
+      @passenger = Passenger.find(params[:id])
+    else
+      forbidden
+    end
   end
 
   def update
-    @passenger.update(passenger_params)
-    @alert = Alert.new(alert_params)
-    @alert.passenger_id = @passenger.id
-    @alert.user_id = current_user.id
-    @alert.solved = false
-    if @alert.save
-      redirect_to passenger_path(@passenger), notice: "Alerta criado com sucesso!"
+    if current_user.admin? || current_user.police?
+      @passenger.update(passenger_params)
+      @alert = Alert.new(alert_params)
+      @alert.passenger_id = @passenger.id
+      @alert.user_id = current_user.id
+      if @alert.save
+        redirect_to passenger_path(@passenger), notice: "Alerta criado com sucesso!"
+      else
+        render :new
+      end
     else
-      render :new
+      forbidden
     end
   end
 
   def destroy
-    @passenger.destroy
-    redirect_to passenger_path
+    if current_user.admin?
+      @passenger.destroy
+      redirect_to passenger_path
+    else
+      forbidden
+    end
   end
 
   private
+
+  def forbidden
+    redirect_to root_path, alert: "Você não pode realizar esta ação."
+  end
 
   def passenger_params
     params.require(:passenger).permit(:full_name, :date_of_birth, :gender, :cpf, :identification_number, :identification_state)
