7e3899fa5d48140dccba6f394bf6beea5f32d208
Mayssaa Khodr <mayus.tayus@gmail.com>
2020-10-07 00:17:56 -0300

    formatação de seat, full_name, length de cpf e dropdown de states em formulários

--- app/models/passenger_trip.rb
+++ app/models/passenger_trip.rb
@@ -4,7 +4,7 @@ class PassengerTrip < ApplicationRecord
 
   # validates :bus_travel, uniqueness: { scope: :passenger }
   validates :seat, :passenger_id, :bus_travel_id, presence: true
-  # validates :seat, length: { is: 2 }
+  # validates :seat, length: { in: (1..48) }
                     # message: "%{value} deve ter 2 caracteres" }
   # validates :seat, :identification_number, format: { with: /0-9]+#$/,
                   # message: "%{value} deve ter apenas números" }
