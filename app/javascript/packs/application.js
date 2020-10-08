153dd09c203e65bf32d8169e5c9aaca6ad3b91cb
Vinicius Viegas <vuviegas85@gmail.com>
2020-10-08 11:17:10 -0300

    Mask inputs implemented and strip cpf before save method implemented

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("../../../vendor/assets/dist/js/dsgov")
 
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -27,12 +28,15 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import 'jquery-mask-plugin';
 import { clickTableRow } from '../components/table';
+import { maskInput } from '../components/mask';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   clickTableRow();
+  maskInput();
 });
 
 // Esta função abaixo veio junto com o Menu lateral, mas ainda não sei por para funcionar
